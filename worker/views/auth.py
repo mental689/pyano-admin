336f4393fb787f4764d6b180d79452ca45d9a8b5
Tuan Anh Nguyen <39395178+mental689@users.noreply.github.com>
2019-01-25 08:29:01 +0900

    Updated profile page for reviewers.

--- worker/views/auth.py
+++ worker/views/auth.py
@@ -5,11 +5,13 @@ from django.shortcuts import redirect, render
 from django.views.generic import View
 from django_comments_xtd.models import Comment
 from django.db.models import Count, Sum, Q, Avg
+from django.db.models.functions import TruncDate
 from survey import models as survey_models
 
 from common.forms import AddWorkerForm
 from search.models import *
 from vatic.models import Solution
+from employer.views.jobs import daterange
 from worker.models import *
 
 logger = logging.getLogger(__name__)
@@ -23,16 +25,18 @@ class ProfileView(View):
     """
     template_name = 'common/profile.html'
 
-    def get(self,request, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
             return redirect(to='{}/login/?next=/accounts/profile'.format(settings.LOGIN_URL))
         if not request.user.is_annotator and not request.user.is_reviewer:
             return redirect(to='/')
         context = {}
         if request.user.is_annotator:
-            this_month_searches = KeywordSearch.objects.filter(worker=request.user, created_at__gte=now()-timedelta(+30))
-            last_month_searches = KeywordSearch.objects.filter(worker=request.user, created_at__gte=now()-timedelta(+60),
-                                                               created_at__lt=now()-timedelta(+30))
+            this_month_searches = KeywordSearch.objects.filter(worker=request.user,
+                                                               created_at__gte=now() - timedelta(+30))
+            last_month_searches = KeywordSearch.objects.filter(worker=request.user,
+                                                               created_at__gte=now() - timedelta(+60),
+                                                               created_at__lt=now() - timedelta(+30))
             context['this_month_searches'] = this_month_searches
             context['last_month_searches'] = last_month_searches
             if last_month_searches.count() == 0:
@@ -41,35 +45,42 @@ class ProfileView(View):
                 context['up_searches'] = '{:.2f} %'.format(
                     (this_month_searches.count() - last_month_searches.count()) / last_month_searches.count() * 100
                 )
-            this_month_suvery_answers = survey_models.Response.objects.filter(created__gte=now()-timedelta(+30),
-                                                                              user=request.user).annotate(earning=Sum('survey__pyano_survey__credits__amount'))
+            this_month_suvery_answers = survey_models.Response.objects.filter(created__gte=now() - timedelta(+30),
+                                                                              user=request.user).annotate(
+                earning=Sum('survey__pyano_survey__credits__amount'))
             last_month_survey_answers = survey_models.Response.objects.filter(created__gte=now() - timedelta(+60),
-                                                                              created__lt=now()-timedelta(+30),
-                                                                              user=request.user).annotate(earning=Sum('survey__pyano_survey__credits__amount'))
+                                                                              created__lt=now() - timedelta(+30),
+                                                                              user=request.user).annotate(
+                earning=Sum('survey__pyano_survey__credits__amount'))
             context['this_month_suvery_answers'] = this_month_suvery_answers
             context['last_month_survey_answers'] = last_month_survey_answers
             context['this_month_earning'] = sum([response.earning for response in this_month_suvery_answers])
             context['last_month_earning'] = sum([response.earning for response in last_month_survey_answers])
             if last_month_survey_answers.count() != 0:
-                context['up_answers'] = '{:.2f} %'.format((this_month_suvery_answers.count()-last_month_survey_answers.count())/last_month_survey_answers.count() * 100)
+                context['up_answers'] = '{:.2f} %'.format((
+                                                                      this_month_suvery_answers.count() - last_month_survey_answers.count()) / last_month_survey_answers.count() * 100)
             else:
                 context['up_answers'] = 'N/A'
             this_month_vatics = Solution.objects.filter(created_at__gte=now() - timedelta(+30),
-                                                        submitter__user=request.user)#.annotate(earning=Sum('job__group__credits__amount'))
+                                                        submitter__user=request.user)  # .annotate(earning=Sum('job__group__credits__amount'))
             last_month_vatics = Solution.objects.filter(created_at__gte=now() - timedelta(+60),
                                                         created_at__lt=now() - timedelta(+30),
-                                                        submitter__user=request.user)#.annotate(earning=Sum('job__group__credits__amount'))
+                                                        submitter__user=request.user)  # .annotate(earning=Sum('job__group__credits__amount'))
             context['this_month_vatics'] = this_month_vatics
             context['last_month_vatics'] = last_month_vatics
-            context['this_month_earning'] += sum([va.job.group.cost if va.job.completed else 0 for va in this_month_vatics])
+            context['this_month_earning'] += sum(
+                [va.job.group.cost if va.job.completed else 0 for va in this_month_vatics])
             context['last_month_earning'] += sum(
                 [va.job.group.cost if va.job.completed else 0 for va in last_month_vatics])
             if last_month_vatics.count() != 0:
-                context['up_vatics'] = '{:.2f} %'.format((this_month_vatics.count() - last_month_vatics.count()) / last_month_vatics.count() * 100)
+                context['up_vatics'] = '{:.2f} %'.format(
+                    (this_month_vatics.count() - last_month_vatics.count()) / last_month_vatics.count() * 100)
             else:
                 context['up_vatics'] = 'N/A'
             if context['last_month_earning'] != 0:
-                context['up_earning'] = '{:.2f} %'.format((context['this_month_earning']-context['last_month_earning'])/context['last_month_earning']*100)
+                context['up_earning'] = '{:.2f} %'.format(
+                    (context['this_month_earning'] - context['last_month_earning']) / context[
+                        'last_month_earning'] * 100)
             else:
                 context['up_earning'] = 'N/A'
             # Comments
@@ -90,13 +101,25 @@ class ProfileView(View):
             this_month_comments = Comment.objects.filter(user=request.user,
                                                          is_removed=False,
                                                          content_type__app_label__contains='vatic',
-                                                         submit_date__gte=now()-timedelta(+30))
+                                                         submit_date__gte=now() - timedelta(+30))
+            this_month_comments_by_date = Comment.objects.filter(user=request.user,
+                                                                 is_removed=False,
+                                                                 content_type__app_label__contains='vatic',
+                                                                 submit_date__gte=now() - timedelta(+30)) \
+                .annotate(day=TruncDate('submit_date')).values('day').annotate(created_count=Count('id'))
+            data = {}
+            for k in this_month_comments_by_date:
+                data[k['day']] = k['created_count']
+            context['this_month_comments_by_date'] = [data[d] if d in data else 0 for d in
+                                                      daterange(start_date=now() - timedelta(+30),
+                                                                end_date=now() + timedelta(+1))]
             last_month_comments = Comment.objects.filter(user=request.user,
                                                          is_removed=False,
                                                          content_type__app_label__contains='vatic',
                                                          submit_date__gte=now() - timedelta(+60),
-                                                         submit_date__lt=now()-timedelta(+30))
-            all_comments = Comment.objects.filter(user=request.user, is_removed=False, content_type__app_label__contains='vatic')
+                                                         submit_date__lt=now() - timedelta(+30))
+            all_comments = Comment.objects.filter(user=request.user, is_removed=False,
+                                                  content_type__app_label__contains='vatic')
             object_pks = list(set([c.object_pk for c in all_comments]))
             this_month_author_responses = Comment.objects.filter(~Q(user=request.user),
                                                                  user__is_annotator=True,
@@ -112,9 +135,11 @@ class ProfileView(View):
                                                                  submit_date__gte=now() - timedelta(+60),
                                                                  submit_date__lt=now() - timedelta(+30))
             if last_month_comments.count() > 0:
-                context['up_comments'] = (this_month_comments.count() - last_month_comments.count())/last_month_comments.count() * 100
+                context['up_comments'] = '{:.2f} %'.format(
+                    (this_month_comments.count() - last_month_comments.count()) / last_month_comments.count() * 100
+                )
             else:
-                context['up_comments'] = 'Last month data is NA'
+                context['up_comments'] = 'N/A'
             context['this_month_comments'] = this_month_comments
             context['last_month_comments'] = last_month_comments
             context['num_comments'] = Comment.objects.filter(user=request.user, is_removed=False).count()
