c6a7fa960768204b9b3a5b0be8218d32ef24f23a
Tuan Anh Nguyen <39395178+mental689@users.noreply.github.com>
2019-02-01 00:18:20 +0900

    bugfix

--- modeling/model.py
+++ modeling/model.py
@@ -30,20 +30,30 @@ def write_command_pwc(v, output_file):
 
 def create_command_for_jobgroup(id):
     jobs = Job.objects.filter(group_id=id, completed=True).all()
+    cmds = []
     for job in tqdm(jobs):
         segment = job.segment
         v = segment.video
         for i in range(segment.start, segment.stop-1):
             cmd = "python3 run.py --model default --first {} --second {} --out {}.flo" \
                 .format(get_frame_path(v, i), get_frame_path(v, i + 1), get_frame_path(v, i))
-            args = shlex.split(cmd)
-            logger.debug(subprocess.run(args, capture_output=True))
+            cmds.append(cmd)
+    return cmds
 
+
+def write_command_for_jobgroup(id, output_file):
+    cmds = create_command_for_jobgroup(id)
+    with open(output_file, "w") as f:
+        f.write("#!/bin/sh\n\n")
+        for cmd in cmds:
+            f.write("echo '{}'\n".format(cmd))
+            f.write(cmd + '\n')
+    f.close()
 
 
 if __name__ == '__main__':
-    create_command_for_jobgroup(id=2)
-    # output_file = '{}/thirdparty/pytorch-pwc/{}.sh'
+    output_file = '{}/thirdparty/pytorch-pwc/{}.sh'
+    write_command_for_jobgroup(id=2, output_file=output_file)
     # videos = Video.objects.all()
     # for v in tqdm(videos):
     #     write_command_pwc(v, output_file=output_file.format(settings.BASE_DIR, v.slug))
