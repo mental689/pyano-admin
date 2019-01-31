56264345b86e031a3a619e927b03c378f9bfdf5d
Tuan Anh Nguyen <39395178+mental689@users.noreply.github.com>
2019-02-01 00:06:32 +0900

    bugfix

--- modeling/__init__.py
+++ modeling/__init__.py
@@ -3,6 +3,6 @@ sys.path.insert(0, os.path.abspath('..'))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'pyano_admin.settings'
 import django
 django.setup()
-from vatic.models import Video
+from vatic.models import Video, Job, JobGroup, Segment
 from django.conf import settings
 import logging
--- modeling/model.py
+++ modeling/model.py
@@ -1,4 +1,6 @@
 from modeling import *
+from tqdm import tqdm
+import subprocess, shlex
 logger = logging.getLogger(__name__)
 
 
@@ -26,8 +28,22 @@ def write_command_pwc(v, output_file):
     f.close()
 
 
+def create_command_for_jobgroup(id):
+    jobs = Job.objects.filter(group_id=id, completed=True).all()
+    for job in tqdm(jobs):
+        segment = job.segment
+        v = segment.video
+        for i in range(segment.start, segment.stop-1):
+            cmd = "python3 run.py --model default --first {} --second {} --out {}.flo" \
+                .format(get_frame_path(v, i), get_frame_path(v, i + 1), get_frame_path(v, i))
+            args = shlex.split(cmd)
+            logger.debug(subprocess.run(args, capture_output=True))
+
+
+
 if __name__ == '__main__':
-    output_file = '{}/thirdparty/pytorch-pwc/{}.sh'
-    videos = Video.objects.all()
-    for v in videos:
-        write_command_pwc(v, output_file=output_file.format(settings.BASE_DIR, v.slug))
+    create_command_for_jobgroup(id=2)
+    # output_file = '{}/thirdparty/pytorch-pwc/{}.sh'
+    # videos = Video.objects.all()
+    # for v in tqdm(videos):
+    #     write_command_pwc(v, output_file=output_file.format(settings.BASE_DIR, v.slug))
