c3178fc8245afb4986702a2240cde37cae7db98b
Tuan Anh Nguyen <39395178+mental689@users.noreply.github.com>
2019-02-02 01:42:03 +0900

    updated training script

--- modeling/i3d/train_i3d.py
+++ modeling/i3d/train_i3d.py
@@ -8,7 +8,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('-mode', type=str, help='rgb or flow')
 parser.add_argument('-save_model', type=str)
-parser.add_argument('-root', type=str)
+parser.add_argument('-split_file', type=str)
 
 args = parser.parse_args()
 
@@ -28,9 +28,10 @@ import numpy as np
 from modeling.i3d.pytorch_i3d import InceptionI3d
 
 from modeling.shoplift import ShopliftDataset as Dataset
+from modeling.shoplift import STATES
 
 
-def run(init_lr=0.1, max_steps=64e3, mode='rgb', root='/ssd/Charades_v1_rgb', train_split='charades/charades.json',
+def run(init_lr=0.1, max_steps=64e3, mode='rgb', train_split='fold_1.json',
         batch_size=8 * 5, save_model=''):
     # setup dataset
     train_transforms = transforms.Compose([videotransforms.RandomCrop(224),
@@ -38,11 +39,11 @@ def run(init_lr=0.1, max_steps=64e3, mode='rgb', root='/ssd/Charades_v1_rgb', tr
                                            ])
     test_transforms = transforms.Compose([videotransforms.CenterCrop(224)])
 
-    dataset = Dataset(train_split, 'training', root, mode, train_transforms)
+    dataset = Dataset(train_split, 'train', mode, train_transforms)
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=36,
                                              pin_memory=True)
 
-    val_dataset = Dataset(train_split, 'testing', root, mode, test_transforms)
+    val_dataset = Dataset(train_split, 'test', mode, test_transforms)
     val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=True, num_workers=36,
                                                  pin_memory=True)
 
@@ -52,11 +53,11 @@ def run(init_lr=0.1, max_steps=64e3, mode='rgb', root='/ssd/Charades_v1_rgb', tr
     # setup the model
     if mode == 'flow':
         i3d = InceptionI3d(400, in_channels=2)
-        i3d.load_state_dict(torch.load('models/flow_imagenet.pt'))
+        i3d.load_state_dict(torch.load('thirdparty/pytorch-i3d/models/flow_imagenet.pt'))
     else:
         i3d = InceptionI3d(400, in_channels=3)
-        i3d.load_state_dict(torch.load('models/rgb_imagenet.pt'))
-    i3d.replace_logits(157)
+        i3d.load_state_dict(torch.load('thirdparty/pytorch-i3d/models/rgb_imagenet.pt'))
+    i3d.replace_logits(len(STATES))
     # i3d.load_state_dict(torch.load('/ssd/models/000920.pt'))
     i3d.cuda()
     i3d = nn.DataParallel(i3d)
@@ -121,7 +122,7 @@ def run(init_lr=0.1, max_steps=64e3, mode='rgb', root='/ssd/Charades_v1_rgb', tr
                     lr_sched.step()
                     if steps % 10 == 0:
                         print('{} Loc Loss: {:.4f} Cls Loss: {:.4f} Tot Loss: {:.4f}'.format(phase, tot_loc_loss / (
-                                    10 * num_steps_per_update), tot_cls_loss / (10 * num_steps_per_update),
+                                10 * num_steps_per_update), tot_cls_loss / (10 * num_steps_per_update),
                                                                                              tot_loss / 10))
                         # save model
                         torch.save(i3d.module.state_dict(), save_model + str(steps).zfill(6) + '.pt')
@@ -129,9 +130,9 @@ def run(init_lr=0.1, max_steps=64e3, mode='rgb', root='/ssd/Charades_v1_rgb', tr
             if phase == 'val':
                 print('{} Loc Loss: {:.4f} Cls Loss: {:.4f} Tot Loss: {:.4f}'.format(phase, tot_loc_loss / num_iter,
                                                                                      tot_cls_loss / num_iter, (
-                                                                                                 tot_loss * num_steps_per_update) / num_iter))
+                                                                                             tot_loss * num_steps_per_update) / num_iter))
 
 
 if __name__ == '__main__':
     # need to add argparse
-    run(mode=args.mode, root=args.root, save_model=args.save_model)
+    run(mode=args.mode, train_split=args.split_file, save_model=args.save_model)
